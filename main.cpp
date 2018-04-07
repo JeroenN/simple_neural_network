817a3c4b17705979d061ae165690d89b32ad6a48
JeroenN <jcaniemendal@gmail.com>
2018-04-08 01:23:12 +0200

    random function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <cassert>
 
 struct connection
 {
@@ -16,6 +18,10 @@ class neuron
 public:
     neuron(unsigned n_outputs);
 private:
+    static double random_weight(void)
+    {
+        return rand() / double(RAND_MAX); //Maybe this function will get bigger, otherwise remove function and write in line.
+    }
     double m_output_value;
     std::vector<connection> m_output_weights;
 
@@ -26,6 +32,7 @@ neuron::neuron(unsigned n_outputs)
     for(unsigned c=0; c< n_outputs; ++c)
     {
         m_output_weights.push_back(connection());
+        m_output_weights.back().weight = random_weight();
     }
 }
 //********************************** Class net **************************
@@ -34,21 +41,33 @@ class net
 {
 public:
     net(const std::vector<unsigned> &topology);
-    void feed_forward(const std::vector<double> &input_values) {};
+    void feed_forward(const std::vector<double> &input_values) ;
     void back_propagation(const std::vector<double> &target_values) {};
     void get_results(std::vector<double> &result_values) const {};
 private:
     std::vector<layer> m_layers; //m_layers[layer_number][neuron_number]
 };
 
+void net::feed_forward(const std::vector<double> &input_values)
+{
+    assert(input_values.size() == m_layers[0].size() );
+}
 net::net(const std::vector<unsigned> &topology)
 {
     //creating rows and columns -> layers and neurons
     unsigned n_layers = topology.size();
-    for(unsigned layer_number=0; layer_number!=n_layers; ++ layer_number)
+    for(unsigned layer_number=0; layer_number<n_layers; ++layer_number)
     {
         m_layers.push_back(layer());
-        unsigned n_outputs = layer_number==topology.size() - 1 ? 0 : topology[layer_number +1];
+        unsigned n_outputs;
+        if(layer_number==topology.size() - 1)
+        {
+            n_outputs =  0;
+        }
+        else
+        {
+            n_outputs =topology[layer_number +1];
+        }
         //using <= for the bias layer
         for(unsigned neuron_number=0; neuron_number<= topology[layer_number]; ++neuron_number)
         {
