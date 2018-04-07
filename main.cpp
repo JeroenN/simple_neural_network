8c06e2033dc109017cbaf68b17f63d9a3452cb00
JeroenN <jcaniemendal@gmail.com>
2018-04-07 22:59:09 +0200

    added neuron class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 #include <vector>
 
-class neuron{};
+struct connection
+{
+  double weight;
+  double delta_weight;
+};
+
+class neuron;
 typedef std::vector<neuron> layer;
+//********************************** Class neuron **************************
+
+class neuron
+{
+public:
+    neuron(unsigned n_outputs);
+private:
+    double m_output_value;
+    std::vector<connection> m_output_weights;
+
+};
+
+neuron::neuron(unsigned n_outputs)
+{
+    for(unsigned c=0; c< n_outputs; ++c)
+    {
+        m_output_weights.push_back(connection());
+    }
+}
+//********************************** Class net **************************
 
 class net
 {
@@ -22,10 +48,11 @@ net::net(const std::vector<unsigned> &topology)
     for(unsigned layer_number=0; layer_number!=n_layers; ++ layer_number)
     {
         m_layers.push_back(layer());
+        unsigned n_outputs = layer_number==topology.size() - 1 ? 0 : topology[layer_number +1];
         //using <= for the bias layer
         for(unsigned neuron_number=0; neuron_number<= topology[layer_number]; ++neuron_number)
         {
-            m_layers.back().push_back(neuron());
+            m_layers.back().push_back(neuron(n_outputs));
             std::cout<< "Neuron created!" << "\n";
         }
 
